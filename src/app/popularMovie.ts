718f9b7b2f954c7e9f112cdbbbc668a2cb4672c8
HiralParmar <parmarhiralr@gmail.com>
2018-02-07 14:14:53 -0800

    Created interface for popular movie list

---
+++ src/app/popularMovie.ts
@@ -0,0 +1,12 @@
+export interface IPopularMovie {
+    vote_count: number;
+    id: number;
+    video: boolean;
+    vote_average: number;
+    title: string;
+    popularity: number;
+    poster_path: string;
+    original_language: string;
+    original_title: string;
+    genre_ids: number[];
+}

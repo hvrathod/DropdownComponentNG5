8cfc3551d900043a6dd4f7e4b03826c880b22d0a
HiralParmar <parmarhiralr@gmail.com>
2018-02-07 19:02:58 -0800

    1. Added Clear button 2. Reset Dropdown when clicked on clear button 3. Added CSS to dropdown

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {FormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
 import { PopularMoviesComponent } from './popular-movies/popular-movies.component';
 import { HttpModule } from '@angular/http';
 import {MoviesService} from './movies.service';
+
 
 @NgModule({
   declarations: [
@@ -14,6 +15,7 @@ import {MoviesService} from './movies.service';
   imports: [
     BrowserModule,
     HttpModule,
+    FormsModule
   ],
   providers: [MoviesService],
   bootstrap: [AppComponent]
--- src/app/popular-movies/popular-movies.component.ts
+++ src/app/popular-movies/popular-movies.component.ts
@@ -2,12 +2,16 @@ import { Component, OnInit } from '@angular/core';
 import {MoviesService} from '../movies.service';
 import {IPopularMovie} from '../popularMovie';
 
+
 @Component({
   selector: 'app-popular-movies',
   templateUrl: './popular-movies.component.html',
   styleUrls: ['./popular-movies.component.css']
 })
 export class PopularMoviesComponent {
+
+  isSelected = false;
+  selectedElement:any= {Name:'Movies'};
 
   constructor(private _movieDataService: MoviesService) {
     this.getMovies();
@@ -22,4 +26,13 @@ export class PopularMoviesComponent {
       error => this.errorMessage = <any>error);
   }
 
+  onChange(deviceValue) {
+    console.log(deviceValue);
+    this.isSelected = true;
+}
+
+  clearDropDown(){
+    this.selectedElement = {Name:'Movies'};
+    console.log("clear drop down");
+  }
 }
